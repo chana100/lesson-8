c51b9cc057c27af4c2fc152301967a26ff4d40d6
chana100 <80358310+chana100@users.noreply.github.com>
2025-06-29 19:57:27 +0300

    Add usersApi.py with user data

---
+++ usersApi.py
@@ -0,0 +1,7 @@
+usersApi = [
+    {"id": 1, "name": "Leanne Graham"},
+    {"id": 2, "name": "Ervin Howell"},
+    {"id": 6, "name": "Mrs. Dennis Schulist"},
+    {"id": 8, "name": "Nicholas Runolfsdottir V"},
+    {"id": 9, "name": "Glenna Reichert"}
+]
